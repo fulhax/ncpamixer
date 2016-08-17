2962c948da9f8be6bcfa132a1808f6780a55ecce
c0r73x <c0r73x@fulhax.nu>
2016-08-17 14:08:11 +0200

    Added fallback-version if not building git version

--- src/pulsemixer.cpp
+++ src/pulsemixer.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <getopt.h>
+#include <string.h>
 #include <pulse/pulseaudio.h>
 
 #include <vector>
@@ -14,15 +15,20 @@ int main(int argc, char *argv[])
     static const struct option longOpts[] = {
         { "version", no_argument, 0, 'v' }
     };
-    
+
     int c;
     int longIndex = 0;
-    
-    while((c = getopt_long(argc, argv, "v", longOpts, &longIndex)) != -1) {
-    
-        switch(c) {
+
+    while ((c = getopt_long(argc, argv, "v", longOpts, &longIndex)) != -1) {
+
+        switch (c) {
             case 'v':
-                printf("ncpamixer v%s\n", GIT_VERSION);
+                if (strlen(GIT_VERSION) > 0) {
+                    printf("ncpamixer git v%s\n", GIT_VERSION);
+                } else {
+                    printf("ncpamixer v%s\n", FALLBACK_VERSION);
+                }
+
                 printf("Build type: %s\n", BUILD_TYPE);
                 printf("Build date: %s\n", BUILD_DATE);
                 printf("\n");
@@ -38,7 +44,7 @@ int main(int argc, char *argv[])
 
     pa.init();
 
-    if(ui.init() > 0) {
+    if (ui.init() > 0) {
         ui.run();
     }
 
--- src/version.hpp
+++ src/version.hpp
@@ -5,4 +5,6 @@ extern const char GIT_VERSION[];
 extern const char BUILD_TYPE[];
 extern const char BUILD_DATE[];
 
+const char FALLBACK_VERSION[] = "1.0";
+
 #endif // VERSION_HPP_
