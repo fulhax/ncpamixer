4be4084367c03430051d626cc2bf948a7313f27b
c0r73x <c0r73x@fulhax.nu>
2019-01-21 21:09:02 +0100

    Bumped fallback version

--- src/version.hpp
+++ src/version.hpp
@@ -5,6 +5,6 @@ extern const char GIT_VERSION[];
 extern const char BUILD_TYPE[];
 extern const char BUILD_DATE[];
 
-const char FALLBACK_VERSION[] = "1.2";
+const char FALLBACK_VERSION[] = "1.3";
 
 #endif // VERSION_HPP_
